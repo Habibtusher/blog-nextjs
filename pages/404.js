0fcc49645135fa9b468d9aa55a151d584590adad
Habibtusher <tusherhrt@gmail.com>
2023-03-13 14:37:37 +0600

    formate error page

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,16 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import img from "../public/errror.png"
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import img from "../public/errror.png";
 const Error = () => {
-    return (
-        <div>
-            <Image src={img} width={1400} height={50}>
-
-            </Image>
-            <Link className='btn btn-primary' href='/'>Back to home</Link>
-        </div>
-    );
+  return (
+    <div>
+      <Image src={img} width={1400} height={50}></Image>
+      <Link className="btn btn-primary" href="/">
+        Back to home
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
